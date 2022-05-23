f674605df040723291ef72921d4c15ccbcad7595
Xeres <294146+xeres@users.noreply.github.com>
2022-05-23 10:26:35 +0900

    feat: Add to install asdf-python, pipx, aws-sam-cli with pipx

--- run_once_before_11-utils.bash
+++ run_once_before_11-utils.bash
@@ -6,4 +6,7 @@ sudo apt-get install -y \
     git \
     zip unzip \
     tofrodos \
-    jq
+    jq \
+    make build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
