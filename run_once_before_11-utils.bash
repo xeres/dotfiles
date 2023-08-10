d5d9ddb17afc51aae5b61bcde6aa86555abd5a21
Xeres <294146+xeres@users.noreply.github.com>
2023-08-10 10:11:28 +0900

    feat: Add wslu for WSL

--- run_once_before_11-utils.bash
+++ run_once_before_11-utils.bash
@@ -7,6 +7,7 @@ sudo apt-get install -y \
     zip unzip \
     tofrodos \
     jq \
+    wslu \
     make build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
