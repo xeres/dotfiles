af5f4cc92db29fb08d191d2176b1655d826b8100
Xeres <294146+xeres@users.noreply.github.com>
2024-06-19 23:47:19 +0900

    fix(docker): Change to non-interactive

--- run_once_before_22-docker.bash
+++ run_once_before_22-docker.bash
@@ -9,7 +9,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     docker-ce \
     docker-ce-cli \
     containerd.io \
